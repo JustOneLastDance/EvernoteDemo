0a67c8c589f7839aea28b5562b6815c63e5ba49a
JustOneLastDance <xss186@126.com>
2016-05-24 17:30:45 +0800

    初始化项目

---
+++ ImitateEvernote/ImitateEvernote/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImitateEvernote
+//
+//  Created by  justinchou on 16/5/24.
+//  Copyright © 2016年  justinchou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImitateEvernote/ImitateEvernote/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImitateEvernote
+//
+//  Created by  justinchou on 16/5/24.
+//  Copyright © 2016年  justinchou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
